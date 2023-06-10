8c1ea574ede387c86a6f9c48d6e2a1c8568a6666
Pallav Avatarit <50916497+theCuriousObserver@users.noreply.github.com>
2023-06-10 18:12:48 +0530

    Added Education and Work

--- src/Resume.js
+++ src/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 const Resume = () => {
@@ -9,46 +9,81 @@ const Resume = () => {
             <VerticalTimeline>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    date="2003 - 2014"
+                    date="February 2023 - Present"
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 >
-                    <h3 className="vertical-timeline-element-title">Jeevan Marg Sophia Secondary School</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Deoria, Uttar Pradesh, India</h4>
+                    <h3 className="vertical-timeline-element-title">Cyber Security Project Manager</h3>
+                    <h4 className="vertical-timeline-element-subtitle">Cyber Waves BV, Amsterdam, Netherlands</h4>
                     <p>
-                        Matriculation, Indian School of Certificate Education (ICSE)
+                        Responsibilities:
+                        <ul>
+                            <li>Responsibility 1</li>
+                            <li>Responsibility 2</li>
+                        </ul>
                     </p>
                 </VerticalTimelineElement>
+
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    date="2014 - 2016"
+                    date="April 2020 - July 2020"
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 >
-                    <h3 className="vertical-timeline-element-title">Delhi Public School</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Ranchi, Jharkhand, India</h4>
+                    <h3 className="vertical-timeline-element-title">Sales & Marketing Intern</h3>
+                    <h4 className="vertical-timeline-element-subtitle">HighRadius, Hyderabad, India</h4>
                     <p>
-                        Intermediate, Central Board of Secondary School (CBSE)
+                        Responsibilities:
+                        <ul>
+                            <li>Responsibility 1</li>
+                            <li>Responsibility 2</li>
+                        </ul>
                     </p>
                 </VerticalTimelineElement>
+
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    date="2017 - 2021"
+                    date="December 2019 - January 2020"
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 >
-                    <h3 className="vertical-timeline-element-title">Kalinga Institute of Industrial Technology</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Bhubaneswar, Odisha, India</h4>
+                    <h3 className="vertical-timeline-element-title">WordPress Developer Intern</h3>
+                    <h4 className="vertical-timeline-element-subtitle">R&C Estate, Cairo, Egypt</h4>
                     <p>
-                        Bachelor of Technology, Information Technology
+                        Responsibilities:
+                        <ul>
+                            <li>Responsibility 1</li>
+                            <li>Responsibility 2</li>
+                        </ul>
                     </p>
                 </VerticalTimelineElement>
+
                 <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2021 - 2022"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    className="vertical-timeline-element--education"
+                    date="September 2021 - September 2022"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
                 >
-                    <h3 className="vertical-timeline-element-title">University of Birmingham</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Birmingham, United Kingdom</h4>
+                    <h3 className="vertical-timeline-element-title">Master of Science, Cyber Security</h3>
+                    <h4 className="vertical-timeline-element-subtitle">University of Birmingham, Birmingham, United Kingdom</h4>
                     <p>
-                        Master of Science, Cyber Security
+                        Achievements:
+                        <ul>
+                            <li>Achievement 1</li>
+                            <li>Achievement 2</li>
+                        </ul>
+                    </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="August 2017 - May 2021"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                >
+                    <h3 className="vertical-timeline-element-title">Bachelor of Technology, Information Technology</h3>
+                    <h4 className="vertical-timeline-element-subtitle">Kalinga Institute of Industrial Technology, Bhubaneswar, Odisha</h4>
+                    <p>
+                        Achievements:
+                        <ul>
+                            <li>Achievement 1</li>
+                            <li>Achievement 2</li>
+                        </ul>
                     </p>
                 </VerticalTimelineElement>
             </VerticalTimeline>
