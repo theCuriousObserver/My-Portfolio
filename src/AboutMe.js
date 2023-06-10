1221f230a2cc64612927d42af961104175184e10
Pallav Avatarit <50916497+theCuriousObserver@users.noreply.github.com>
2023-06-10 11:12:31 +0530

    Heading went on Top and an underline added

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -6,9 +6,9 @@ const AboutMe = () => {
     return (
         <div className="about-me" id="about-me">
             <StarrySky />
+            <h1 className="about-me-heading">Know Me More</h1>
             <div className="about-me-content">
-                <h1 className="about-me-heading">Know Me More</h1>
-                <div className="about-me-text">
+                    <div className="about-me-text">
                     <h2 className="about-me-subheading">I'm <span className="highlight">Pallav Avatarit</span>, a Cyber Security Professional</h2>
                     <p>
                         I am a seasoned Cyber Security Specialist at Cyber Waves BV in the Netherlands. With a master's degree from the University of Birmingham, I have comprehensive expertise in data security, network configuration, OT/IT security, incident response, and threat mitigation. My strong communication skills allow for effective collaboration, while my attention to detail ensures potential threats are addressed. I am adaptable, flexible, and committed to continuous learning to stay abreast of industry trends. Alongside my technical skills, I excel in time management and organization, consistently delivering exceptional customer service and tailored security solutions.
