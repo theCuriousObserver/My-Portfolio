0405cda77009977c5e44bd4cd8ceddb0a290491c
Pallav Avatarit <50916497+theCuriousObserver@users.noreply.github.com>
2023-06-19 15:55:48 +0530

    Updated Contact Details

--- src/sections/ContactMe.js
+++ src/sections/ContactMe.js
@@ -6,30 +6,33 @@ import tryhackmeIcon from '../assets/icons/tryhackme.svg';
 
 const ContactMe = () => {
   return (
-    <div className="contact-me" id="contact-me">
-      <h1 className="contact-me-heading">Contact Me</h1>
-      
-      <div className="contact-me-content">
-        
-        {/* Left Column */}
-        <div className="contact-me-left">
-          <div className="contact-details">
-            <p>Email: pallav.avatarit12@gmail.com</p>
-            <p>Phone: +91 7077105233</p>
-          </div>
+      <div className="contact-me" id="contact-me">
+          <h1 className="contact-me-heading">Contact Me</h1>
           
-          <div className="social-icons">
-            <a href="https://www.linkedin.com/in/pallav-avatarit" target="_blank" rel="noreferrer">
-              <img src={linkedinIcon} alt="LinkedIn" />
-            </a>
-            <a href="https://github.com/theCuriousObserver" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
-            </a>
-            <a href="https://tryhackme.com/p/pallav.avatarit" target="_blank" rel="noreferrer">
-              <img src={tryhackmeIcon} alt="TryHackMe" />
-            </a>
-          </div>
-        </div>
+          <div className="contact-me-content">
+              {/* Left Column */}
+              <div className="contact-me-left">
+                  {/* Add subheading for Contact Details */}
+                  <h2 className="contact-details-subheading">Contact Details</h2>
+                  <div className="contact-details">
+                      <p>Email: pallav.avatarit12@gmail.com</p>
+                      <p>Phone: +91 7077105233</p>
+                  </div>
+                  
+                  {/* Add subheading for Follow Me */}
+                  <h2 className="follow-me-subheading">Follow Me</h2>
+                  <div className="social-icons">
+                      <a href="https://www.linkedin.com/in/pallav-avatarit" target="_blank" rel="noreferrer">
+                          <img src={linkedinIcon} alt="LinkedIn" />
+                      </a>
+                      <a href="https://github.com/theCuriousObserver" target="_blank" rel="noreferrer">
+                          <img src={githubIcon} alt="GitHub" />
+                      </a>
+                      <a href="https://tryhackme.com/p/theCuriousObserver" target="_blank" rel="noreferrer">
+                          <img src={tryhackmeIcon} alt="TryHackMe" />
+                      </a>
+                  </div>
+              </div>
         
         {/* Right Column */}
         <div className="contact-me-right">
