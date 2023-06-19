fb4be50899d1d5607d56c3db560d6868237a62fb
Pallav Avatarit <50916497+theCuriousObserver@users.noreply.github.com>
2023-06-19 16:16:00 +0530

    Update to the ContactMe Section

--- src/sections/ContactMe.js
+++ src/sections/ContactMe.js
@@ -5,50 +5,48 @@ import githubIcon from '../assets/icons/github.svg';
 import tryhackmeIcon from '../assets/icons/tryhackme.svg';
 
 const ContactMe = () => {
-  return (
-      <div className="contact-me" id="contact-me">
-          <h1 className="contact-me-heading">Contact Me</h1>
-          
-          <div className="contact-me-content">
-              {/* Left Column */}
-              <div className="contact-me-left">
-                  {/* Add subheading for Contact Details */}
-                  <h2 className="contact-details-subheading">Contact Details</h2>
-                  <div className="contact-details">
-    <p><img src="../assets/icons/gmail.png" alt="Email Icon" /> Email: pallav.avatarit12@gmail.com</p>
-    <p><img src="../assets/icons/phone-call.png" alt="Phone Icon" /> Phone: +91 7077105233</p>
-</div>
-
-                  
-                  {/* Add subheading for Follow Me */}
-                  <h2 className="follow-me-subheading">Follow Me</h2>
-                  <div className="social-icons">
-                      <a href="https://www.linkedin.com/in/pallav-avatarit" target="_blank" rel="noreferrer">
-                          <img src={linkedinIcon} alt="LinkedIn" />
-                      </a>
-                      <a href="https://github.com/theCuriousObserver" target="_blank" rel="noreferrer">
-                          <img src={githubIcon} alt="GitHub" />
-                      </a>
-                      <a href="https://tryhackme.com/p/theCuriousObserver" target="_blank" rel="noreferrer">
-                          <img src={tryhackmeIcon} alt="TryHackMe" />
-                      </a>
-                  </div>
-              </div>
-        
-        {/* Right Column */}
-        <div className="contact-me-right">
-          <form className="contact-form">
-            <input type="text" placeholder="Name" required />
-            <input type="email" placeholder="Email" required />
-            <input type="text" placeholder="Subject" required />
-            <textarea placeholder="Message" required></textarea>
-            <button type="submit">Send</button>
-          </form>
+    return (
+        <div className="contact-me" id="contact-me">
+            <h1 className="contact-me-heading">Contact Me</h1>
+            
+            <div className="contact-me-content">
+                {/* Left Column */}
+                <div className="contact-me-left">
+                    {/* Add subheading for Contact Details */}
+                    <h2 className="contact-details-subheading">Contact Details</h2>
+                    <div className="contact-details">
+                        <p><img src="../assets/icons/gmail.png" alt="Email Icon" className="contact-icon" /> Email: pallav.avatarit12@gmail.com</p>
+                        <p><img src="../assets/icons/phone-call.png" alt="Phone Icon" className="contact-icon" /> Phone: +91 7077105233</p>
+                    </div>
+                    
+                    {/* Add subheading for Follow Me */}
+                    <h2 className="follow-me-subheading">Follow Me</h2>
+                    <div className="social-icons">
+                        <a href="https://www.linkedin.com/in/pallav-avatarit" target="_blank" rel="noreferrer">
+                            <img src={linkedinIcon} alt="LinkedIn" />
+                        </a>
+                        <a href="https://github.com/theCuriousObserver" target="_blank" rel="noreferrer">
+                            <img src={githubIcon} alt="GitHub" />
+                        </a>
+                        <a href="https://tryhackme.com/p/theCuriousObserver" target="_blank" rel="noreferrer">
+                            <img src={tryhackmeIcon} alt="TryHackMe" />
+                        </a>
+                    </div>
+                </div>
+                
+                {/* Right Column */}
+                <div className="contact-me-right">
+                    <form className="contact-form">
+                        <input type="text" placeholder="Name" required />
+                        <input type="email" placeholder="Email" required />
+                        <input type="text" placeholder="Subject" required />
+                        <textarea placeholder="Message" required></textarea>
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ContactMe;
